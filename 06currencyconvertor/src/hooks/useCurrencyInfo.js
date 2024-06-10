df4310768fecf7c6d1ae2d8ddd2fb802c96c4194
Yash Bhise <ybhise0911@gmail.com>
2024-06-10 15:25:40 +0530

    currencyconverter

---
+++ 06currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,36 @@
+import {useEffect,useState} from 'react'
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+
+        ).then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo;
+
+
+
+
+// import {useEffect, useState} from "react"
+
+
+// function useCurrencyInfo(currency){
+//     const [data, setData] = useState({})
+//     useEffect(() => {
+//         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+//         .then((res) => res.json())
+//         .then((res) => setData(res[currency]))
+//         console.log(data);
+//     }, [currency])
+//     console.log(data);
+//     return data
+// }
+
+// export default useCurrencyInfo;
